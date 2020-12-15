58c1558cc04440336f6244fcc9eb4d35d02448be
tuankiet64 <trantuankiet071097@gmail.com>
2020-12-15 19:42:16 +0700

    fix dup chart

--- public/js/component/app/chart/index.js
+++ public/js/component/app/chart/index.js
@@ -2,13 +2,13 @@ var ctx = $('#myChart');
 $(function () {
     var d = new Date();
     var n = d.getFullYear();
-    date='1-1-'+n+'';
+    date = '1-1-' + n + '';
     $('#calendar-start').datetimepicker({
         // viewMode: 'years',
         format: 'MM-YYYY',
         locale: 'vi',
-        minDate:date,
-        maxDate:'12-31-'+n+'',
+        minDate: date,
+        maxDate: '12-31-' + n + '',
         icons: {
             next: "icofont icofont-rounded-right",
             previous: "icofont icofont-rounded-left"
@@ -18,33 +18,41 @@ $(function () {
         // viewMode: 'years',
         format: 'MM-YYYY',
         locale: 'vi',
-        minDate:date,
-        maxDate:'12-31-'+n+'',
+        minDate: date,
+        maxDate: '12-31-' + n + '',
         icons: {
             next: "icofont icofont-rounded-right",
             previous: "icofont icofont-rounded-left"
         }
     })
-    loadchart()
+    getdata()
 })
-function loadchart() {
-    var datestart=$('#calendar-start').val(),
-        dateend=$('#calendar-end').val();
+var myChart=null;
+function drawchart(config) {
+    if(myChart!=null){
+        myChart.destroy();
+    }
+    myChart = new Chart(ctx, config)
+}
+
+function getdata() {
+    var datestart = $('#calendar-start').val(),
+        dateend = $('#calendar-end').val();
     $.ajax({
-        type:'get',
-        url:'/chart.getdata',
-        data:{datestart:datestart,dateend:dateend}
+        type: 'get',
+        url: '/chart.getdata',
+        data: {datestart: datestart, dateend: dateend}
     }).then(function (res) {
-        var myChart = new Chart(ctx, {
+        let data = {
             type: 'bar',
             data: {
                 labels: res[1],
                 datasets: res[0]
             },
             options: {
-                legend:{
+                legend: {
                     display: true,
-                    align:'center'
+                    align: 'center'
                 },
                 title: {
                     display: true,
@@ -58,38 +66,40 @@ function loadchart() {
                     }]
                 }
             }
-        });
-        $('#calendar-start').on('dp.change',function () {
-            valuestart=$(this).val()
-            monthstart=valuestart.substr(0,2);
-            valueend=$('#calendar-end').val()
-            monthend=valueend.substr(0,2);
-            if(monthstart>monthend){
-                text='Ngày bắt đầu không được lớn hơn ngày cuối';
-                ErrorNotify(text)
-                $(this).val(valueend)
-                return false;
-            }else{
-                myChart.destroy()
-                loadchart()
-            }
+        };
+        drawchart(data)
+        // myChart = new Chart(ctx,config)
 
-        })
-        $('#calendar-end').on('dp.change',function () {
-            valuestart=$(this).val()
-            monthstart=valuestart.substr(0,2);
-            valueend=$('#calendar-start').val()
-            monthend=valueend.substr(0,2);
-            if(monthstart<monthend){
-                text='Ngày cuối không được nhỏ hơn ngày đầu';
-                ErrorNotify(text)
-                $(this).val(valueend)
-                return false;
-            }else{
-                myChart.destroy()
-                loadchart()
-            }
-        })
     })
 }
 
+$('#calendar-start').on('dp.change', function () {
+    valuestart = $(this).val()
+    monthstart = valuestart.substr(0, 2);
+    valueend = $('#calendar-end').val()
+    monthend = valueend.substr(0, 2);
+    if (monthstart > monthend) {
+        text = 'Ngày bắt đầu không được lớn hơn ngày cuối';
+        ErrorNotify(text)
+        $(this).val(valueend)
+        getdata()
+        return false;
+    } else {
+        getdata()
+    }
+})
+$('#calendar-end').on('dp.change', function () {
+    valuestart = $(this).val()
+    monthstart = valuestart.substr(0, 2);
+    valueend = $('#calendar-start').val()
+    monthend = valueend.substr(0, 2);
+    if (monthstart < monthend) {
+        text = 'Ngày cuối không được nhỏ hơn ngày đầu';
+        ErrorNotify(text)
+        $(this).val(valueend)
+        getdata()
+        return false;
+    } else {
+        getdata()
+    }
+})
